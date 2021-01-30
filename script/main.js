9522fc13f154dc5bf5c73f56ddfcf93eb70828e2
Pallav Priyam <32682467+pallavpriyam@users.noreply.github.com>
2021-01-30 16:55:20 +0530

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -114,7 +114,9 @@ const animationTimeline = () => {
     })
     .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
     .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
+    .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
+    .from(".idea-41", 0.7, ideaTextTrans)
+    .to(".idea-41", 0.7, ideaTextTransLeave, "+=1.5")
     .from(
       ".idea-5",
       0.7,
